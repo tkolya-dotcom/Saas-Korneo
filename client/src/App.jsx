41763312433dbe200889f911d1055ff67e2b52ff
tkolya-dotcom <244952613+tkolya-dotcom@users.noreply.github.com>
2026-04-13 18:08:58 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'import Layout from './components/Layout'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
 import Dashboard from './pages/Dashboard'
 import Tables from './pages/Tables'
 import TableView from './pages/TableView'
@@ -14,21 +15,23 @@ import Connections from './pages/Connections'
 
 export default function App() {
   return (
-    <Routes>
-            <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Tables />} />
-        <Route path="tables" element={<Tables />} />
-        <Route path="table/:tableName" element={<TableView />} />
-        <Route path="table/:tableName/new" element={<TableNew />} />
-        <Route path="table/:tableName/edit/:id" element={<TableEdit />} />
-        <Route path="schema" element={<SchemaView />} />
-        <Route path="schema/editor" element={<SchemaEditor />} />
-        <Route path="sql" element={<SqlRunner />} />
-        <Route path="backup" element={<Backup />} />
-        <Route path="migration" element={<Migration />} />
-        <Route path="policies" element={<Policies />} />
-        <Route path="connections" element={<Connections />} />
-                </Route>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Tables />} />
+          <Route path="tables" element={<Tables />} />
+          <Route path="table/:tableName" element={<TableView />} />
+          <Route path="table/:tableName/new" element={<TableNew />} />
+          <Route path="table/:tableName/edit/:id" element={<TableEdit />} />
+          <Route path="schema" element={<SchemaView />} />
+          <Route path="schema/editor" element={<SchemaEditor />} />
+          <Route path="sql" element={<SqlRunner />} />
+          <Route path="backup" element={<Backup />} />
+          <Route path="migration" element={<Migration />} />
+          <Route path="policies" element={<Policies />} />
+          <Route path="connections" element={<Connections />} />
+        </Route>
       </Routes>
-                  </Router>
+    </Router>
+  )
+}
